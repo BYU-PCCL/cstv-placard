a371ae63a9e624648d75a18a30a77ad7ff6f08fc
chris-luangrath <thechrisluangrath@gmail.com>
2021-06-16 13:50:44 -0600

    removed comments

--- time.js
+++ time.js
@@ -6,16 +6,11 @@ var x = setInterval(function () {
     var min = date.getMinutes();
     var sec = date.getSeconds();
   
-    // var n = date.toLocaleTimeString();
-    // var n = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
     var n = date.toLocaleTimeString([], {timeStyle: 'short'}).toLowerCase();
     var noSpace = n.split(" ");
     n = noSpace[0] + noSpace[1];
 
     var current_time = new Date().getTime()/1000;
-    // console.log("endTime: " + endTime)
-    // console.log("current_Time: " + current_time)
-    // console.log("endTime - current: " + (endTime - current_time))
     if((endTime - current_time) < 11 && (endTime - current_time) > 0){
         if(endTime - current_time > 0){
             n = "00:" + (Math.floor(endTime - current_time).toString().padStart(2,'0')) + "  ";
