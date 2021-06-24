4f6de729bf7e1427482fb583d7be6e53107d895b
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-24 15:37:36 -0600

    Reformat with prettier

--- main.js
+++ main.js
@@ -1,79 +1,79 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const fs = require('fs')
-const express = require('express')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
 
-const {env} = process;
-const runtimePath = path.join(env.XDG_RUNTIME_DIR, "placard")
-const socketPath = path.join(runtimePath, "socket")
+const { env } = process;
+const runtimePath = path.join(env.XDG_RUNTIME_DIR, "placard");
+const socketPath = path.join(runtimePath, "socket");
 
 let win;
 
 const content = {
-    title: "Starting...",
-    description: null,
-    artist: null,
-    url: null,
+  title: "Starting...",
+  description: null,
+  artist: null,
+  url: null,
 };
 
 async function createServer() {
-    const server = express();
-    server.use(express.json())
-    server.patch("/placard", async (req, res) => {
-        const body = await req.body;
+  const server = express();
+  server.use(express.json());
+  server.patch("/placard", async (req, res) => {
+    const body = await req.body;
 
-        for (let key in content) {
-            const bodyVal = body[key];
-            if (bodyVal === undefined) {
-                continue;
-            }
+    for (let key in content) {
+      const bodyVal = body[key];
+      if (bodyVal === undefined) {
+        continue;
+      }
 
-            content[key] = bodyVal
-        }
-
-        win.webContents.send("updateContent", content);
-        res.status(200).json({status: "ok"});
-    });
-    server.get("/placard", async (_, res) => {
-        res.status(200).json(content);
-    });
-
-    if (!fs.existsSync(runtimePath)) {
-        fs.mkdirSync(runtimePath)
-    } else if (fs.existsSync(socketPath)) {
-        // It's probably a bad practice to just delete the file but it should work practically
-        fs.rmSync(socketPath)
+      content[key] = bodyVal;
     }
 
-    server.listen(socketPath)
+    win.webContents.send("updateContent", content);
+    res.status(200).json({ status: "ok" });
+  });
+  server.get("/placard", async (_, res) => {
+    res.status(200).json(content);
+  });
+
+  if (!fs.existsSync(runtimePath)) {
+    fs.mkdirSync(runtimePath);
+  } else if (fs.existsSync(socketPath)) {
+    // It's probably a bad practice to just delete the file but it should work practically
+    fs.rmSync(socketPath);
+  }
+
+  server.listen(socketPath);
 }
 
 async function createWindow() {
-    win = new BrowserWindow({
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true,
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.setAlwaysOnTop(true, 'floating')
+  win = new BrowserWindow({
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  win.setAlwaysOnTop(true, "floating");
 
-    await win.loadFile('index.html')
+  await win.loadFile("index.html");
 
-    win.webContents.send("updateContent", content)
+  win.webContents.send("updateContent", content);
 }
 
 app.whenReady().then(() => {
-    setTimeout(async function () {
-        await createWindow();
-        await createServer();
-    }, 1000);
-})
+  setTimeout(async function () {
+    await createWindow();
+    await createServer();
+  }, 1000);
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- renderer.js
+++ renderer.js
@@ -1,46 +1,48 @@
-const { ipcRenderer } = require('electron')
-const QRCode = require('qrcode')
+const { ipcRenderer } = require("electron");
+const QRCode = require("qrcode");
 
-const lockImagePath = "img/lock_black_48dp.svg"
+const lockImagePath = "img/lock_black_48dp.svg";
 
-const titleElement = document.querySelector("#title")
-const descriptionElement = document.querySelector("#description")
-const artistElement = document.querySelector("#artist")
-const qrImageElement = document.querySelector("#qr")
+const titleElement = document.querySelector("#title");
+const descriptionElement = document.querySelector("#description");
+const artistElement = document.querySelector("#artist");
+const qrImageElement = document.querySelector("#qr");
 
 // https://stackoverflow.com/a/11765731/1979008
-function setQRCode(url){
-    if (url == null)  {
-        qrImageElement.src = lockImagePath;
-        qrImageElement.classList.add("locked")
-        return;
-    }
+function setQRCode(url) {
+  if (url == null) {
+    qrImageElement.src = lockImagePath;
+    qrImageElement.classList.add("locked");
+    return;
+  }
 
-    qrImageElement.classList.remove("locked")
+  qrImageElement.classList.remove("locked");
 
-    const svg = QRCode.toString(url, {type: "svg"}, (error) => {
-        if (error) alert(error)
-    })
-    const blob = new Blob([svg], {type: 'image/svg+xml'});
-    const imageUrl = URL.createObjectURL(blob);
-    qrImageElement.crossOrigin = "Anonymous";
-    qrImageElement.src = imageUrl
-    qrImageElement.addEventListener('load', () => URL.revokeObjectURL(imageUrl), {once: true});
+  const svg = QRCode.toString(url, { type: "svg" }, (error) => {
+    if (error) alert(error);
+  });
+  const blob = new Blob([svg], { type: "image/svg+xml" });
+  const imageUrl = URL.createObjectURL(blob);
+  qrImageElement.crossOrigin = "Anonymous";
+  qrImageElement.src = imageUrl;
+  qrImageElement.addEventListener("load", () => URL.revokeObjectURL(imageUrl), {
+    once: true,
+  });
 }
 
-ipcRenderer.on('updateContent', (event, args) => {
-    const {title, description, artist, url} = args;
-    titleElement.innerHTML = title;
-    descriptionElement.innerHTML = description;
+ipcRenderer.on("updateContent", (event, args) => {
+  const { title, description, artist, url } = args;
+  titleElement.innerHTML = title;
+  descriptionElement.innerHTML = description;
 
-    if (artist != null) {
-        artistElement.style.display = "initial";
-        artistElement.innerHTML = artist;
-    } else {
-        artistElement.style.display = "none";
-    }
+  if (artist != null) {
+    artistElement.style.display = "initial";
+    artistElement.innerHTML = artist;
+  } else {
+    artistElement.style.display = "none";
+  }
 
-    if (url !== undefined) {
-        setQRCode(args.url)
-    }
+  if (url !== undefined) {
+    setQRCode(args.url);
+  }
 });
